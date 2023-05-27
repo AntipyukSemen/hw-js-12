1a3747e0e12777ceea1f9173a4a49212ba0a0f70
AntipyukSemen <110487377+AntipyukSemen@users.noreply.github.com>
2023-05-27 15:51:33 +0300

    hw-js-12

---
+++ js/function-4.js
@@ -0,0 +1,5 @@
+// const btnRef = document.querySelector('[data-button]');
+// function btnClick () {
+//     alert('Hello World!');
+// };
+// btnRef.addEventListener('click', btnClick);
---
+++ js/function-6.js
@@ -0,0 +1,17 @@
+// const applyCallbackToEachElement = function (arr, callback) {
+//   return callback(arr);
+// };
+
+// const array = [1, 2, 3, 4, 5];
+
+// const squareCallback = function (arr) {
+//   const newArr = [];
+//   for (let arrElement of arr) {
+//     newArr.push(arrElement * arrElement)
+//   };
+//   return newArr;
+// };
+
+// const result = applyCallbackToEachElement(array, squareCallback);
+
+// console.log(result);
